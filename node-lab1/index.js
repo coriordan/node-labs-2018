decea62b939ad50f351aa6b5b62b77d151c07de0
Cathal O’Riordan <cathal@madein75.com>
2018-04-05 13:29:11 +0100

    Changed to express app

--- node-lab1/index.js
+++ node-lab1/index.js
@@ -1,25 +1,30 @@
-import http from 'http';
 import dotenv from 'dotenv';
-import greeting from './greetings.js';
+import express from 'express';
+import greeting from './greetings';
 
 dotenv.config();
 
+const app = express();
+
 const port = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+app.use(express.static('public'));
+
+app.get('/greeting', (req, res) => {
   let lang = req.headers['accept-language'];
   const defaultLang = 'en';
   if (!greeting[lang]) lang = defaultLang;
-
   const response = {
     lang: lang,
     message: greeting[lang],
   };
-  res.writeHead(200, {'Content-type': 'text/plain',
-                      'Content-language': response.lang});
+
+  res.writeHead(200, {'Content-Type': 'text/plain',
+                      'Content-Language': response.lang});
   res.end(response.message);
 });
 
-server.listen(port);
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
 
-console.log(`Server running at ${port}`);
